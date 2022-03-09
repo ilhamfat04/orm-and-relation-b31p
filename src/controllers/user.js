393046793b204b15f3fdf43ad69bd0d7a23c7d29
= <=>
2022-03-09 09:12:05 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,25 @@
 // import model here
+const { user } = require('../../models')
 
-exports.addUsers = async (req, res) => {
+exports.addUser = async (req, res) => {
     // code here
+    try {
+        const data = req.body
+
+        // const query = `INSERT INTO users () VALUES ()`
+        // await db.sequelize.query(query)
+
+        const createdData = await user.create(data)
+
+        res.send({
+            status: "success",
+            data: createdData
+        })
+    } catch (error) {
+        console.log(error);
+        res.send({
+            status: "Failed",
+            message: "Server error"
+        })
+    }
 }
