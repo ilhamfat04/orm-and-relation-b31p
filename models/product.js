0549632b8a6d68592ad42ebdac2541b3d00d11c1
= <=>
2022-03-09 11:47:56 +0700

    update

--- models/product.js
+++ models/product.js
@@ -24,6 +24,15 @@ module.exports = (sequelize, DataTypes) => {
       });
 
       // code here
+      product.belongsToMany(models.category, {
+        as: "categories",
+        through: {
+          model: "productCategory",
+          as: "bridge"
+        },
+        foreignKey: "idProduct"
+      })
+
     }
   }
   product.init(
